fec5c1c51912ece6fe84e507a2aa3537b1b97dfa
Ricardo Torres Panting <jrtorrespanting@hotmail.com>
2020-10-16 03:00:58 -0500

    Added comments for better readability

--- text_neural_network/functions/functions.go
+++ text_neural_network/functions/functions.go
@@ -51,7 +51,7 @@ func LoadFile(file string) (*mat.Dense, *mat.Dense, []string, []string) {
 }
 
 func LoadIntens(file string) Outmost {
-	// load our calculated synapse values
+	// load our intents file
 	jsonFile, err := os.Open(file)
 	// if we os.Open returns an error then handle it
 	if err != nil {
@@ -62,7 +62,7 @@ func LoadIntens(file string) Outmost {
 
 	//read our opened json file
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	//Initialize map
+	//Initialize data for storing the intents
 	var data Outmost
 	err = json.Unmarshal([]byte(byteValue), &data)
 	if err != nil {
@@ -72,6 +72,7 @@ func LoadIntens(file string) Outmost {
 }
 
 func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dropout bool, dropout_percent float64, words_db []string, categories []string) {
+	//We get the dimension of input x and y
 	rx, cx := x.Dims()
 	ry, cy := y.Dims()
 	fmt.Printf("Training with %v,  alpha: %f, dropout: %t\n", hidden, alpha, dropout)
@@ -90,28 +91,32 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 	for i := range data2 {
 		data2[i] = 2*rand.Float64() - 1
 	}
-	//Set random weights
+	//Set random weights for synapse_1 and synapse_0
 	synapse_0 := mat.NewDense(c1, hidden, data1)
 	synapse_1 := mat.NewDense(hidden, c2, data2)
 	prev_synapse_0_weight_update := new(mat.Dense)
 	prev_synapse_0_weight_update.CloneFrom(synapse_0)
 	prev_synapse_0_weight_update.Zero()
 
+	//Set matrix of zeros of same size as synapse_1
 	prev_synapse_1_weight_update := new(mat.Dense)
 	prev_synapse_1_weight_update.CloneFrom(synapse_1)
 	prev_synapse_1_weight_update.Zero()
 
+	//Set matriz of zeros of same size as synapse_0
 	synapse_0_direction_count := new(mat.Dense)
 	synapse_0_direction_count.CloneFrom(synapse_0)
 	synapse_0_direction_count.Zero()
 
+	//Set matrix of zeros of same size as synapse_1
 	synapse_1_direction_count := new(mat.Dense)
 	synapse_1_direction_count.CloneFrom(synapse_1)
 	synapse_1_direction_count.Zero()
 
 	for ep := 0; ep <= epochs; ep++ {
-		//Feed forward through layer 0, layer 1, and layer 2
+		//Set input, layer 0
 		layer_0 := x
+		//Feed forward to layer 1
 		layer_0_prod := new(mat.Dense)
 		layer_0_prod.Product(layer_0, synapse_0)
 		layer_1 := sigmoid(layer_0_prod)
@@ -125,11 +130,12 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 			ones := mat.NewDense(c1, hidden, data3)
 			layer_1.Product(layer_1, ones)
 		}
-
+		//Forward propagation to layer 2
 		layer_2_prod := new(mat.Dense)
 		layer_2_prod.Product(layer_1, synapse_1)
 		layer_2 := sigmoid(layer_2_prod)
 
+		//Get error of layer_2
 		layer_2_error := new(mat.Dense)
 		layer_2_error.Sub(y, layer_2)
 
@@ -137,29 +143,37 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 			//Check error
 			var mean_err float64
 			mean_err = mat_mean(absolute(layer_2_error))
+			//If error is decreasing all GOOD, continue
 			if mean_err < last_mean_error {
 				fmt.Printf("delta after %v, iterations: %f\n", ep, mean_err)
 				last_mean_error = mean_err
 			} else {
+				//If error is getting bigger then something is wrong, stop the process
 				fmt.Printf("break, delta: %f > last_mean_error: %f\n", mean_err, last_mean_error)
 				break
 			}
 		}
+		//Get delta of layer_2 from the error
 		layer_2_delta := new(mat.Dense)
 		layer_2_delta.MulElem(layer_2_error, sig_to_dev(layer_2))
 
+		//Get layer_1 error
 		layer_1_error := new(mat.Dense)
 		layer_1_error.Product(layer_2_delta, synapse_1.T())
 
+		//Get delta of layer_1 from error
 		layer_1_delta := new(mat.Dense)
 		layer_1_delta.MulElem(layer_1_error, sig_to_dev(layer_1))
 
+		//Get the updated weights from a the delta of layers
 		synapse_1_weight_update := new(mat.Dense)
 		synapse_0_weight_update := new(mat.Dense)
 		synapse_1_weight_update.Product(layer_1.T(), layer_2_delta)
 		synapse_0_weight_update.Product(layer_0.T(), layer_1_delta)
 
+		//If this is not the first loop then
 		if ep > 0 {
+			//Apply a biniarization on the synapse 0 matrix
 			synapse_0_binary := new(mat.Dense)
 			prev_synapse_0_binary := new(mat.Dense)
 			synapse_0_binary.Apply(func(i, j int, v float64) float64 {
@@ -171,6 +185,7 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 					return v
 				}
 			}, synapse_0_weight_update)
+			//Apply a binarization on the prev synapse 0 matrix
 			prev_synapse_0_binary.Apply(func(i, j int, v float64) float64 {
 				if v > 0 {
 					v = 1
@@ -180,10 +195,12 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 					return v
 				}
 			}, prev_synapse_0_weight_update)
+			//Substract both matrix to get the new direction
 			sub_0_result := new(mat.Dense)
 			sub_0_result.Sub(synapse_0_binary, prev_synapse_0_binary)
 			synapse_0_direction_count.Add(synapse_0_direction_count, absolute(sub_0_result))
 
+			//Apply a binarization on the synapse 1 matrix
 			synapse_1_binary := new(mat.Dense)
 			prev_synapse_1_binary := new(mat.Dense)
 			synapse_1_binary.Apply(func(i, j int, v float64) float64 {
@@ -195,6 +212,7 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 					return v
 				}
 			}, synapse_1_weight_update)
+			//Apply a binarizaion on the prev synapse matrix
 			prev_synapse_1_binary.Apply(func(i, j int, v float64) float64 {
 				if v > 0 {
 					v = 1
@@ -204,134 +222,164 @@ func Train(x *mat.Dense, y *mat.Dense, hidden int, alpha float64, epochs int, dr
 					return v
 				}
 			}, prev_synapse_1_weight_update)
+			//Substract both matrix to get the new direction
 			sub_1_result := new(mat.Dense)
 			sub_1_result.Sub(synapse_1_binary, prev_synapse_1_binary)
 			synapse_1_direction_count.Add(synapse_1_direction_count, absolute(sub_1_result))
 
 		}
+		//Apply the learning rate alpha to synapse 1 matrix
 		mul := new(mat.Dense)
 		mul.Apply(func(i, j int, v float64) float64 { return alpha * v }, synapse_1_weight_update)
 		synapse_1.Add(synapse_1, mul)
 
+		//Aply learning rate alpha to synapse 0 matrix
 		mul1 := new(mat.Dense)
 		mul1.Apply(func(i, j int, v float64) float64 { return alpha * v }, synapse_0_weight_update)
 		synapse_0.Add(synapse_0, mul1)
 
+		//Upadte previous synapse with the most recent value
 		prev_synapse_0_weight_update.CloneFrom(synapse_0_weight_update)
 		prev_synapse_1_weight_update.CloneFrom(synapse_1_weight_update)
 
 	}
-	//Persist Synapses
+	//Store the synapse matrixes values on data
 	data := synapse{
 		Synapse_0:  synapse_0.RawMatrix(),
 		Synapse_1:  synapse_1.RawMatrix(),
 		Words:      words_db,
 		Categories: categories,
 	}
+	//Encode the data into a json
 	file, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 	}
+	//Save the file into model.json
 	_ = ioutil.WriteFile("model.json", file, 0644)
 }
 
 func Classify(sentence string, details bool, synapse_0 *mat.Dense, synapse_1 *mat.Dense, words []string, categories []string) Entries {
 	var result *mat.Dense
+	//Get the prediction of the ANN, and save it
 	result = think(sentence, details, synapse_0, synapse_1, words)
+	//Get the columns of result
 	_, c := result.Dims()
 	prediction := make(map[int]float64)
+	//Iterate thorugh all categories and identify the ones greater than ERROR_THRESHOLD
 	for i := 0; i < c; i++ {
 		if result.At(0, i) > ERROR_THRESHOLD {
+			//Save the ones that have a high certainity value
 			prediction[i] = result.At(0, i)
 		}
 	}
 	var es Entries
+	//Iterate through the map to identify the value and the index of the prediciton
 	for k, v := range prediction {
+		//Get the corresponding category from the categoies array
 		es = append(es, Entry{Val: v, Key: categories[k]})
 	}
-
-	sort.Sort(sort.Reverse(es))
-	fmt.Printf("Input: %s\n Category: %v Confidence: %v\n", sentence, es[0].Key, es[0].Val)
+	if es != nil {
+		fmt.Printf("Input: %s\n Category: %v Confidence: %v\n", sentence, es[0].Key, es[0].Val)
+	} else {
+		es = append(es, Entry{Val: 99.99, Key: categories[10]})
+		fmt.Printf("Input: %s\n Category: %v Confidence: %v\n", sentence, es[0].Key, es[0].Val)
+	}
+	//Get the response based on the identified category
 	answer := response(es)
+	fmt.Printf("Output: %v\n", answer[0].Key)
 	return answer
 }
 
 func think(sentence string, details bool, synapse_0 *mat.Dense, synapse_1 *mat.Dense, words []string) *mat.Dense {
+	//Given a sentence, get the binary vector according to the words used, and the word on the data base
 	x := bow(sentence, words, details)
 	if details {
 		fmt.Println("sentence:", sentence, "\nbow:", x)
 	}
-	//Input Layer
+	//Input the binarized sentence as fisrt Layer
 	l0 := x
-	//Matrix multiplication Intput and Hidden
+	//Matrix multiplication Intput and Hidden layer
 	d := new(mat.Dense)
 	d.Product(l0, synapse_0)
 	l1 := sigmoid(d)
 	d1 := new(mat.Dense)
 	d1.Product(l1, synapse_1)
+	//Output layer, response of the newtwork
 	l2 := sigmoid(d1)
-
 	return l2
 }
 
+//This function is going to set the split rule, to ")"
 func PhraseSplitFunc(data []byte, atEOF bool) (advance int, token []byte, err error) {
-
+	//If we are at the end of file and there no  data
 	if atEOF && len(data) == 0 {
+		//Then we finished
 		return 0, nil, nil
 	}
-
+	//We search for ")" on the data, if it is after the first character of data
 	if i := strings.Index(string(data), ")"); i >= 0 {
+		//Then we get all the characters before ") and if this is not empty"
 		if string(data[0:i]) != "" {
+			//We then move on after the ")" and return all the data before ")"
 			return i + 1, data[0 : i+1], nil
 		}
 	}
-
+	//If we are at end of file
 	if atEOF {
+		//We return the remain data, and the index after this data
 		return len(data), data, nil
 	}
 
 	return
 }
 
+//This function split our file in lines of sentences,category
 func ScanPhrases(path string) ([]string, error) {
-
+	//We open the database file
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
-
+	//No matter what we close it
 	defer file.Close()
-
+	//We start a new sacnner of our database file
 	scanner := bufio.NewScanner(file)
-
+	//We split our file in lines according to the custom PhraseSplitFunction
 	scanner.Split(PhraseSplitFunc)
 
 	var phrase []string
-
+	//We scan all inside the file
 	for scanner.Scan() {
+		//Store each phrase and category inside a slice
 		phrase = append(phrase, scanner.Text())
 	}
 	return phrase, err
 }
 
+//This function is going get every word remove accents and remove articles and unnecesary words
 func scanWords(list string) []string {
-
+	//We start a new scanner
 	scanner := bufio.NewScanner(strings.NewReader(list))
-
+	//We split according to spaces
 	scanner.Split(bufio.ScanWords)
 	var boolean bool
 	var words []string
 	ign := []string{"la", "a", "un", "una", "?", "!", "el", "con", "sin", "en", "para",
 		"por", ".", "siempre", "desde", "los", "las", "me", "que", "tan", "de", "favor"}
-
+	//We scan for every word
 	for scanner.Scan() {
 		text := scanner.Text()
+		//We find if the word is inside ignored database
 		boolean, _ = Find(ign, strings.ToLower(text))
 		if boolean {
+			//If true then we ignore it
 			continue
 		} else {
+			//If it is not, then we remove accents
 			t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
 			result, _, _ := transform.String(t, text)
+			//Save it on the word data base
 			words = append(words, strings.ToLower(result))
 		}
 	}
@@ -339,58 +387,65 @@ func scanWords(list string) []string {
 	return words
 }
 
-func SetDb(line []string, db map[string][]string) map[string][]string {
-
-	re1 := regexp.MustCompile(`\((.+)\)`)
-	re2 := regexp.MustCompile(`\#(.+)\s\(`)
-	db = make(map[string][]string)
-	for _, word := range line {
-		id := string(re1.FindAllSubmatch([]byte(word), -1)[0][1])
-		if id != "noanswer" {
-			text := string(re2.FindAllSubmatch([]byte(word), -1)[0][1])
-			db[id] = append(db[id], text)
-		} else {
-			db[id] = append(db[id], "")
-
-		}
-
-	}
-	return db
-}
-
-func Organize(db map[string][]string) ([]string, []string) {
-	var boolean bool
-	count := 0
-	keys := []string{}
-
-	for k, _ := range db {
-		count += len(db[k])
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
+//This function set our data base correcly, on a map in the way category:sentences
+//And get a word_database of al unique words of all sentences
+//And a category database of all categories in the database
+func SetDb(line []string) (map[string][]string, []string, []string) {
 	words := []string{}
 	categories := []string{}
-
-	for _, k := range keys {
-		for _, item := range db[k] {
-			w := scanWords(item)
-			for _, wrd := range w {
-				boolean, _ = Find(words, wrd)
-				if !boolean {
-					words = append(words, wrd)
-				}
+	keys := []string{}
+	var id, text string
+	var boolean bool
+	//Set the REGEX rules for category
+	re1 := regexp.MustCompile(`\((.+)\)`)
+	//Set the regex rule for sentence
+	re2 := regexp.MustCompile(`\#(.+)\s\(`)
+	//Initialize database map
+	db := make(map[string][]string)
+	//Iterate through every word in the line
+	for _, sentence := range line {
+		//Identify the category
+		id = string(re1.FindAllSubmatch([]byte(sentence), -1)[0][1])
+		if id != "noanswer" {
+			//Identify the sentence
+			text = string(re2.FindAllSubmatch([]byte(sentence), -1)[0][1])
+			//Save the category and sentence
+			db[id] = append(db[id], text)
+			//If the category is "noanswer" just insert an "" as sentence
+		} else {
+			db[id] = append(db[id], "")
+		}
+		//Get an array of every word on the sentence
+		w := scanWords(text)
+		//Iterate through all words of the sentence
+		for _, wrd := range w {
+			//Find if wrd is already in words
+			boolean, _ = Find(words, wrd)
+			if !boolean {
+				//If wrd not in words then add it
+				words = append(words, wrd)
 			}
 		}
-		boolean, _ = Find(categories, k)
-		if !boolean {
-			categories = append(categories, k)
-		}
 	}
-	return words, categories
+	//Get an array with all categories
+	for k, _ := range db {
+		keys = append(keys, k)
+	}
+	//Sort keys on alphabetical order
+	sort.Strings(keys)
+	for _, k := range keys {
+		//Assign categories in alphabetical order to variable categories
+		categories = append(categories, k)
+	}
+
+	return db, words, categories
 }
 
+//This function finds if a string is already present on a slice
 func Find(slice []string, val string) (bool, int) {
+	//Iterate throug index of a slice
 	for i, _ := range slice {
+		//Compare if the string on i index is equal to val string
 		if slice[i] == val {
 			return true, i
 		}
@@ -398,37 +453,51 @@ func Find(slice []string, val string) (bool, int) {
 	return false, 0
 }
 
+//This function change our sentences database into a binary array of sentences
 func Binarize(db map[string][]string, word_db []string, categories []string) (*mat.Dense, *mat.Dense) {
 
 	count := 0
 	keys := []string{}
-
+	//We get an array of keys of the database map
 	for k, _ := range db {
 		count += len(db[k])
 		keys = append(keys, k)
 	}
+	//We sort them in alphabetical order
 	sort.Strings(keys)
 
+	//Initialize zero matrix of sentences(training) and categories(output)
 	training := mat.NewDense(count, len(word_db), nil)
 	output := mat.NewDense(count, len(categories), nil)
 
 	i := 0
+	//Iteration through every category
 	for _, k := range keys {
 		pattern_words := []string{}
-		for _, word := range db[k] {
-			pattern_words = scanWords(word)
+		//Iteration through every sentence
+		for _, sentence := range db[k] {
+			//Get all words in the sentence
+			pattern_words = scanWords(sentence)
+			//For every word in slice of words
 			for _, w := range pattern_words {
+				//For every word in word database
 				for j, item := range word_db {
+					//If both are the same
 					if item == w {
+						//Assign a one
 						training.Set(i, j, 1)
 					}
 				}
 			}
+			//For every category
 			for q, item := range categories {
+				//If they are equal
 				if item == k {
+					//Assign a one
 					output.Set(i, q, 1)
 				}
 			}
+			//Move to the next row on the matrix
 			i++
 		}
 	}
@@ -438,11 +507,14 @@ func Binarize(db map[string][]string, word_db []string, categories []string) (*m
 func response(category Entries) Entries {
 	var sentence string
 	var v int
+	//Load intents from file
 	intents_db := LoadIntens("C:\\Users\\jrtor\\go\\src\\text_neural_network\\intents.json")
-
+	//Search for the correct category
 	switch category[0].Key {
 	case "greeting":
+		//Generate a random number
 		v = rand.Intn(len(intents_db.Category.Greeting))
+		//Choose a phrase of index v from array
 		sentence = intents_db.Category.Greeting[v]
 	case "goodbye":
 		v = rand.Intn(len(intents_db.Category.Goodbye))
@@ -481,41 +553,49 @@ func response(category Entries) Entries {
 		v = rand.Intn(len(intents_db.Category.Liked))
 		sentence = intents_db.Category.Liked[v]
 	}
-
+	//Save sentence inside es, with actual value of centainty
 	var es Entries
 	es = append(es, Entry{Val: category[0].Val, Key: sentence})
 	return es
 }
 
+//This function applies the function sigmoid elemnt wise to a matrix
 func sigmoid(v *mat.Dense) *mat.Dense {
+	//Get matrix dimension
 	r, c := v.Dims()
+	//Initialize matrix output
 	output := mat.NewDense(r, c, nil)
-	output.Apply(sig, v)
+	//Apply sigmoid function to every element of v
+	output.Apply(func(i, j int, v float64) float64 { return 1.0 / (1.0 + math.Exp(-1*v)) }, v)
 	return output
 }
-func sig(i, j int, v float64) float64 {
-	return 1.0 / (1.0 + math.Exp(-1*v))
-}
 
+//This function applies the second derivate of sigmoid function to matrix v
 func sig_to_dev(v *mat.Dense) *mat.Dense {
+	//Initialize matrix m
 	m := new(mat.Dense)
+	//Initialize matrix output
 	output := new(mat.Dense)
+	//Apply "1-v" element wise to every element in v
 	m.Apply(func(i, j int, v float64) float64 { return (1 - v) }, v)
+	//Multiply element wise every element of v and m
 	output.MulElem(v, m)
 	return output
 }
 
-func clean_up_sentence(sentence string) []string {
-	sentence_words := scanWords(sentence)
-	return sentence_words
-}
-
+//This function binarize the input sentence to be able to insert it to the NN
 func bow(sentence string, words_db []string, details bool) *mat.Dense {
-	sentence_words := clean_up_sentence(sentence)
+	//Get every word of the sentence
+	sentence_words := scanWords(sentence)
+	//Initialize slice of float64
 	bag := make([]float64, len(words_db))
-	for _, s := range sentence_words {
+	//Iterate through every word of the sentence
+	for _, word := range sentence_words {
+		//Iterate through every word in words database
 		for i, item := range words_db {
-			if item == s {
+			//If they are equal
+			if item == word {
+				//Set a one
 				bag[i] = 1
 			}
 		}
@@ -523,40 +603,36 @@ func bow(sentence string, words_db []string, details bool) *mat.Dense {
 	if details {
 		fmt.Println(bag)
 	}
+	//Create new vector from slice
 	v := mat.NewDense(1, len(bag), bag)
 	return v
 }
 
+//This function gets the absolute value of a matrix
 func absolute(m1 *mat.Dense) *mat.Dense {
+	//Initialize matrix
 	m_abs := new(mat.Dense)
+	//Apply math.abs to every element in m1
 	m_abs.Apply(func(i, j int, v float64) float64 { return math.Abs(v) }, m1)
 	return m_abs
 }
 
+//This function gets the mean of a matrix
 func mat_mean(m1 *mat.Dense) float64 {
+	//Get dimensions of m1
 	r, c := m1.Dims()
 	var val float64
+	//Iterate through every element in the matrix
 	for i := 0; i < r-1; i++ {
 		for j := 0; j < c-1; j++ {
+			//Add every value
 			val += m1.At(i, j)
 		}
 	}
+	//Divide total sum and total numbers
 	mean := val / float64(r*c)
 	return mean
 }
-
-//func word_checker(word string, db_word string) float64 {
-//	count := 0
-//		for _, glet := range db_word {
-//			if ulet == glet {
-//			} else {
-//			}
-//		}
-//
-//	}
-//	similarity := float64(count) / float64(len(db_word))
-//	return similarity
-//}
 
 type Entry struct {
 	Val float64
--- text_neural_network/neural_network.go
+++ text_neural_network/neural_network.go
@@ -24,35 +24,36 @@ var dropout_percent = 0.2
 var details = false
 
 func main() {
-	//Separamos el archivo de texto en lineas
+	//Separate file in lines
 	line, err := functions.ScanPhrases("./chatss.txt")
 	if err != nil {
 		panic(err)
 	}
-	//Creamos variable de base de datos
-	//Analizamos cada linea del texto separada, para dientificar el mensaje y la clasificacion
-	training_data = functions.SetDb(line, training_data)
-
-	var words []string
-	var categories []string
-	words, categories = functions.Organize(training_data)
-
+	//Get the training data, words database and categroies database from our lines database
+	training_data, words, categories = functions.SetDb(line)
+	//Get the corresponding binary matrix of every sentences database, and categories database
 	training, output = functions.Binarize(training_data, words, categories)
-
-	command := flag.String("command", "train", "Either train or test to evaluate neural network")
-	user_input := flag.String("user_input", "quiero una pizza", "Type a sentence for the chat bot")
+	//Set flag to be able to decide from cmd, train or test
+	command := flag.String("command", "test", "Either train or test to evaluate neural network")
+	//Set flag to be able to set a test sentence from cmd
+	user_input := flag.String("user_input", "lloro", "Type a sentence for the chat bot")
 	flag.Parse()
 
-	// train or  predict to determine the effectiveness of the trained network
+	// train the network or test to determine the effectiveness of the trained network
 	switch *command {
 	case "train":
+		//Start time
 		rand.Seed(time.Now().UTC().UnixNano())
 		t1 := time.Now()
+		//Train the database
 		functions.Train(training, output, hidden_neurons, alpha, epochs, dropout, dropout_percent, words, categories)
+		//End time
 		elapsed := time.Since(t1)
 		fmt.Printf("\nTime taken to train: %s\n", elapsed)
 	case "test":
+		//Load synapses, word database and categories database
 		synapse_0, synapse_1, words, categories := functions.LoadFile("model.json")
+		//Classify user input from cmd
 		functions.Classify(*user_input, details, synapse_0, synapse_1, words, categories)
 	default:
 		// don't do anything
